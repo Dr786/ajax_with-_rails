e11e151e873117a52a2275ebb003d7753ed621b3
Shakil Akbar <shakil.akbar@yahoo.com>
2021-12-19 19:51:33 +0500

    updated ajax for facts

--- app/assets/javascripts/ajax.js
+++ app/assets/javascripts/ajax.js
@@ -1,8 +1,28 @@
+// Use the DOM to add a collection of facts to the page.
+function addFactsToPage(facts) {
+  const factsDiv = document.querySelector('.facts');
+
+  for (const fact of facts) {
+    const factP = document.createElement('p');
+    factP.innerText = fact.content;
+    factsDiv.appendChild(factP);
+  }
+}
+
+// Makes it so that fetch requests that do not return 200 OK
+// are dealt with as errors.
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
 // Run the fetch request once the DOM has loaded.
 document.addEventListener('DOMContentLoaded', () => {
-
-  fetch('https://dog.ceo/api/breeds/list/all').then(result => {
-    return result.json();
-  });
-  
+  fetch('/facts.json')
+    .then(handleErrors)
+    .then(result => result.json()) // Deserialize the JSON string into a Javascript object.
+    .then(addFactsToPage)
+    .catch(error => alert(error)); // A better stragety for error mitigation is recommended. :)
 });
