d6f137810fbd41852ca0395500195020b948191b
Shakil Akbar <shakil.akbar@yahoo.com>
2021-12-20 20:34:18 +0500

    Added the item scaffold and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_18_152143) do
+ActiveRecord::Schema.define(version: 2021_12_20_153217) do
 
   create_table "facts", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.text "Name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
